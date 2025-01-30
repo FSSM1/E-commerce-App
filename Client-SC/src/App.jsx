d9288dc16a6715a0b8d31b8231c42c916dd07971
Firas Jebri <fiiraas.jeebrii@gmail.com>
2025-01-30 16:05:53 +0100

    fixing imports

--- Client-SC/src/App.jsx
+++ Client-SC/src/App.jsx
@@ -1,40 +1,46 @@
 import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import Contact from "./pages/Contact";
-import HomeLayout from "./pages/HomeLayout";
+import Contact from "./pages/client/Contact";
+import HomeLayout from "./pages/client/HomeLayout";
 import ErrorElement from "./components/ErrorElement";
-import Landing from "./pages/Landing";
-import Allproduct from "./components/products/Allproduct"
-import Signup from "./pages/Signup"
+import Landing from "./pages/client/Landing";
+import Allproduct from "./components/productsClient/Allproduct";
+import Signup from "./pages/client/Signup";
+import Profile from "./pages/client/Profile";
 
 const router = createBrowserRouter([
   {
-    path: '/', 
+    path: "/",
     element: <HomeLayout />,
-    errorElement: <ErrorElement/>, 
+    errorElement: <ErrorElement />,
     children: [
       {
-        path: 'home', 
+        path: "home",
         element: <Landing />,
         errorElement: <ErrorElement />,
       },
       {
-        path:'products', 
+        path: "products",
         element: <Allproduct />,
         errorElement: <ErrorElement />,
       },
       {
-        path:'contact', 
+        path: "contact",
         element: <Contact />,
         errorElement: <ErrorElement />,
       },
       {
-        path:'signup', 
+        path: "signup",
         element: <Signup />,
         errorElement: <ErrorElement />,
       },
-    ]
+      {
+        path: "profile",
+        element: <Profile />,
+        errorElement: <ErrorElement />,
+      },
+    ],
   },
 ]);
 
