65f2ba0a0c47b1cbf97d52c48eebecd1329bb63a
Firas Jebri <fiiraas.jeebrii@gmail.com>
2025-01-30 15:07:28 +0100

    version fixed & architecture backend updated

--- Server/database/models/users.js
+++ Server/database/models/users.js
@@ -29,7 +29,7 @@ module.exports = (sequelize, DataTypes) => {
     },
 
     role: {
-      type: DataTypes.ENUM("admin", "user", "seller"),
+      type: DataTypes.ENUM("admin", "client", "seller"),
       defaultValue: "user",
       allowNull: false,
     },
