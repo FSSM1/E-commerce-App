116b363681db41d4f586b42b9b02e2693d6323cf
Firas Jebri <fiiraas.jeebrii@gmail.com>
2025-01-30 17:34:49 +0100

    test2

--- Client-SC/src/App.jsx
+++ Client-SC/src/App.jsx
@@ -1,96 +1,88 @@
 import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import ContactClient from "../src/pages/client/Contact"
-import ContactSeller from "../src/pages/seller/Contact"
+import ContactClient from "../src/pages/client/Contact";
+import ContactSeller from "../src/pages/seller/Contact";
 
 import HomeLayoutClient from "../src/pages/client/HomeLayout";
 import HomeLayoutSeller from "../src/pages/seller/HomeLayout";
 
+import LandingClient from "../src/pages/client/Landing";
+import LandingSeller from "../src/pages/seller/Landing";
 
-import LandingClient from "../src/pages/client/Landing"
-import LandingSeller from "../src/pages/seller/Landing"
-
-import ProductClient from  "./pages/client/Products"
+import ProductClient from "./pages/client/Products";
 import ProductsSeller from "./pages/seller/Products";
-
-
-
-
-
 
 import ErrorElement from "./components/ErrorElement";
 
-import Allproduct from "./components/products/Allproduct"
-import Signup from "./pages/Signup"
+import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 
 const router = createBrowserRouter([
   {
-    path: '/client', 
+    path: "/client",
     element: <HomeLayoutClient />,
-    errorElement: <ErrorElement/>, 
+    errorElement: <ErrorElement />,
     children: [
       {
-        path: 'Landing', 
+        path: "Landing",
         element: <LandingClient />,
         errorElement: <ErrorElement />,
       },
       {
-        path:'products', 
+        path: "products",
         element: <ProductClient />,
         errorElement: <ErrorElement />,
       },
       {
-        path:'contact', 
+        path: "contact",
         element: <ContactClient />,
         errorElement: <ErrorElement />,
       },
       {
-        path:'signup', 
+        path: "signup",
         element: <Signup />,
         errorElement: <ErrorElement />,
       },
       {
-        path:'login', 
+        path: "login",
         element: <Login />,
         errorElement: <ErrorElement />,
       },
-    ]
+    ],
   },
 
   {
-    path: '/seller', 
+    path: "/seller",
     element: <HomeLayoutSeller />,
-    errorElement: <ErrorElement/>, 
+    errorElement: <ErrorElement />,
     children: [
       {
-        path: 'home', 
+        path: "home",
         element: <LandingSeller />,
         errorElement: <ErrorElement />,
       },
       {
-        path:'products', 
+        path: "products",
         element: <ProductsSeller />,
         errorElement: <ErrorElement />,
       },
       {
-        path:'contact', 
+        path: "contact",
         element: <ContactSeller />,
         errorElement: <ErrorElement />,
       },
       {
-        path:'signup', 
+        path: "signup",
         element: <Signup />,
         errorElement: <ErrorElement />,
       },
       {
-        path:'login', 
+        path: "login",
         element: <Login />,
         errorElement: <ErrorElement />,
-        
       },
-    ]
+    ],
   },
 ]);
 
